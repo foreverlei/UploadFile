5582b1412b34ce8d199e82770fc74f165b3225b0
Lei <516292026@qq.com>
2019-09-11 12:30:02 +0800

    提交备份

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ var storage = multer.diskStorage({
         dir = dir + "/uploads"
         console.log(dir)
         fs.mkdirSync(dir)
+      }else{
+        dir = dir + "/uploads"
       }
       cb(null, dir)
     },
@@ -43,13 +45,42 @@ app.post('/upload-single', upload.single('logo'), function(req, res, next){
     console.log('原始文件名：%s', file.originalname);
     console.log('文件大小：%s', file.size);
     console.log('文件保存路径：%s', file.path);
+    
 
     res.send({ret_code: '0',path:file.path});
 });
 
+// 单图上传
+app.post('/upload-single', upload.single('logo'), function(req, res, next){
+    var file = req.file;
+
+    console.log('文件类型：%s', file.mimetype);
+    console.log('原始文件名：%s', file.originalname);
+    console.log('文件大小：%s', file.size);
+    console.log('文件保存路径：%s', file.path);
+    
+
+    res.send({ret_code: '0',path:file.path});
+});
+// 多图上传
+app.post('/upload-multi', upload.array('logo'), function(req, res, next){
+    var files = req.files;
+    let paths = []
+    files.map((file)=>{
+        console.log('文件类型：%s', file.mimetype);
+        console.log('原始文件名：%s', file.originalname);
+        console.log('文件大小：%s', file.size);
+        console.log('文件保存路径：%s', file.path);
+        paths.push(file.path)
+    })
+    res.send({ret_code: '0',paths});
+});
 app.get('/form', function(req, res, next){
     var form = fs.readFileSync('./form.html', {encoding: 'utf8'});
     res.send(form);
 });
-
+app.get('/forms', function(req, res, next){
+    var form = fs.readFileSync('./forms.html', {encoding: 'utf8'});
+    res.send(form);
+});
 app.listen(3000);
